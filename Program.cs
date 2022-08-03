e7b0286e95f11cbdd7c650eca529fec79db0a05d
Rafael Ramires <moreira.ramires@gmail.com>
2022-08-03 17:06:17 -0300

    criacao de um cronometro

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Menu();
+
+static void Menu(){
+Console.Clear();
+    Console.WriteLine("S = segundos");
+    Console.WriteLine("M = minutos");
+    string respostaUser = Console.ReadLine().ToUpper();
+
+    switch(respostaUser){
+        case "S": Start(1);break;
+        case "M": Start(60);break;
+        default: 
+            Console.WriteLine("Error, tente novamente");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Menu();
+        break;
+    }
+}
+
+static void Start(int seconds){
+    
+    Console.WriteLine("Qual tempo?");
+    float time = float.Parse(Console.ReadLine());
+    
+
+    for( float i= 0; i <= (time*seconds); i++ ){
+        Console.Clear();
+        Console.WriteLine(i);
+        Thread.Sleep(1000);
+    };
+
+        Console.Clear();
+        Console.WriteLine("Contagem finalizada.");
+        Thread.Sleep(3000);
+        Console.Clear();
+        Menu();
+
+
+}
